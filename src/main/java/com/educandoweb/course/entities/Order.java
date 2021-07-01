d59e106296c24ebd8e7b8e6387b9fd51d63ff03a
Gabriel Miller <gabrielmillerone@hotmail.com>
2021-07-01 11:42:17 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -99,7 +99,15 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems(){
 		return items;
 	}
-
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x: items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
